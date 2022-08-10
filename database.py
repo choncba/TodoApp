3e0b9c1807b3dfa0f40ecd60be4375c1fa3e3c1e
choncba <luciano_bono@hotmail.com>
2022-08-10 18:05:00 -0300

    Use Heroku enviroment variable

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlite3 import connect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -6,7 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # Para usar la BD local SQLite3
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"  
 # Para usar Postgres del Add-on de Heroku
-SQLALCHEMY_DATABASE_URL = "postgres://ebtnxpyyomksvd:2c1194ffbf15a921dc4b535d70abe517d96cbe01b571c0bbba8425bfc2ec9f1d@ec2-34-193-44-192.compute-1.amazonaws.com:5432/d2120uf9k2ljha"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
