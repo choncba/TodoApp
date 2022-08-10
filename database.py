cf80fd3d914a196d0ffa7cd8c651f75fc4514261
choncba <luciano_bono@hotmail.com>
2022-08-10 17:33:11 -0300

    Switch to SQLite3

--- database.py
+++ database.py
@@ -2,23 +2,10 @@ from sqlite3 import connect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import pymysql
 
-MYSQL_URL = "ae-media-mysql.cb0wbmrrr5hp.us-east-1.rds.amazonaws.com"
-MYSQL_USER = "u518765"
-MYSQL_PASSWORD = "Chon082022"
-MYSQL_DB_NAME = "todoapp"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
-SQLALCHEMY_DATABASE_URL =   "mysql+pymysql://" + \
-                            MYSQL_USER + \
-                            ":" + \
-                            MYSQL_PASSWORD + \
-                            "@" + \
-                            MYSQL_URL + \
-                            "/" + \
-                            MYSQL_DB_NAME
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
