5e98edf0c86bc037e3362af74e896ebc1bd16aaa
choncba <luciano_bono@hotmail.com>
2022-08-11 16:18:56 -0300

    Check

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
 # TodoApp
 ## Testing FastAPI full stack APP deploying to Heroku
+### En Heroku https://dashboard.heroku.com/apps/choncba-todoapp-deploy
--- database.py
+++ database.py
@@ -6,13 +6,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Para usar la BD local SQLite3
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./data/todos.db"
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 # Para usar Postgres del Add-on de Heroku con sus credenciales, saco las mismas de las variables de entorno
 # Corrijo la URL de Heroku para que lo tome bien sqlalchemy
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
