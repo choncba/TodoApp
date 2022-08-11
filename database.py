9818d3b5690b4e055b5394ed6d4413fec9b845bd
choncba <choncba@gmail.com>
2022-08-11 01:13:14 -0300

    FIX database.py

--- database.py
+++ database.py
@@ -6,15 +6,17 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Para usar la BD local SQLite3
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"  
-# Para usar Postgres del Add-on de Heroku
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Para usar Postgres del Add-on de Heroku con sus credenciales, saco las mismas de las variables de entorno
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 # Corrijo la URL de Heroku para que lo tome bien
 if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
